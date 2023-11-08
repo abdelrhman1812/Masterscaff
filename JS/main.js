2e9b867d926e0580a0c12686139bfedc9c07c5b8
WRQenawy2023 <git config --global user.name WRQenawy2023>
2023-11-08 09:46:32 +0200

    first-

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,5 @@
 
-
+/* increment Number in section in home page */
 let nums=document.querySelectorAll(".num")
 console.log(nums)
 
@@ -23,7 +23,7 @@ nums.forEach((num)=>{
 
 })
 
-/* Slider Projects */
+/*======================= Slider Projects id ================================== */
 let number = 0;
 let parentImg = document.getElementById("parent");
 let sliderImages = Array.from(document.querySelectorAll(".child"));
@@ -62,68 +62,134 @@ function moveNegative() {
 
 
 
-/* ===================== Projects ===============  */
+/* ===================== Home - Projects display image when click img ===============  */
 
 
-// let imgInfo = document.querySelector(".infoo");
-// let imgHtml = document.querySelector(".imgHtml");
-// let iClose = document.querySelector(".close");
-// let iLeft = document.querySelector(".left");
-// let iRight = document.querySelector(".right");
-// let allImages = Array.from(document.querySelectorAll(".img"));
-// console.log(allImages)
-// let counter = 0;
-// // console.log(imgInfo);
-// // console.log(allImages);
-// // console.log(iLeft);
-// // console.log(iRight);
-// // console.log(iClose);
+let imgInfo = document.querySelector(".infoo");
+let imgHtml = document.querySelector(".imgHtml");
+let iClose = document.querySelector(".close");
+let iLeft = document.querySelector(".left");
+let iRight = document.querySelector(".right");
+let allImages = Array.from(document.querySelectorAll(".img"));
+console.log(allImages)
+let counter = 0;
+// console.log(imgInfo);
+// console.log(allImages);
+// console.log(iLeft);
+// console.log(iRight);
+// console.log(iClose);
 
-// for (let i = 0; i < allImages.length; i++) {
-//   //   console.log(allImages[i]);
-//   allImages[i].addEventListener("click", function (e) {
-//     imgInfo.classList.remove("d-none");
-//     console.log(imgInfo)
+for (let i = 0; i < allImages.length; i++) {
+  //   console.log(allImages[i]);
+  allImages[i].addEventListener("click", function (e) {
+    imgInfo.classList.remove("d-none");
+    console.log(imgInfo)
 
-//     let getSrc = e.target.getAttribute("src");
-//     console.log(getSrc);
+    let getSrc = e.target.getAttribute("src");
+    console.log(getSrc);
 
-//     imgHtml.style.backgroundImage = `url(${getSrc})`;
+    imgHtml.style.backgroundImage = `url(${getSrc})`;
 
-//     let indexOfImg = allImages.indexOf(e.target);
-//     counter = indexOfImg;
-//     // console.log(indexOfImg);
-//   });
-// }
+    let indexOfImg = allImages.indexOf(e.target);
+    counter = indexOfImg;
+    // console.log(indexOfImg);
+  });
+}
 
-// function moveplus() {
-//   counter++;
+function movepluss() {
+  counter++;
 
-//   if (counter == allImages.length) {
-//     counter = 0;
-//   }
+  if (counter == allImages.length) {
+    counter = 0;
+  }
 
-//   let srcAllImage = allImages[counter].getAttribute("src");
-//   imgHtml.style.backgroundImage = `url(${srcAllImage})`;
-//   //   console.log(counter);
-// }
+  let srcAllImage = allImages[counter].getAttribute("src");
+  imgHtml.style.backgroundImage = `url(${srcAllImage})`;
+  //   console.log(counter);
+}
 
-// function moveNegative() {
-//   counter--;
+function moveNegativee() {
+  counter--;
 
-//   if (counter == -1) {
-//     counter = allImages.length - 1;
-//   }
-//   let srcAllImage = allImages[counter].getAttribute("src");
-//   imgHtml.style.backgroundImage = `url(${srcAllImage})`;
-//   //   console.log(counter);
-// }
+  if (counter == -1) {
+    counter = allImages.length - 1;
+  }
+  let srcAllImage = allImages[counter].getAttribute("src");
+  imgHtml.style.backgroundImage = `url(${srcAllImage})`;
+  //   console.log(counter);
+}
 
-// iLeft.addEventListener("click", moveNegative);
-// iRight.addEventListener("click", moveplus);
-// iClose.addEventListener("click", function () {
-//   imgInfo.classList.add("d-none");
-// });
+iLeft.addEventListener("click", moveNegativee);
+iRight.addEventListener("click", movepluss);
+iClose.addEventListener("click", function () {
+  imgInfo.classList.add("d-none");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+/* Change Backgroung  */
+
+function changeColor() {
+  let navbar = document.getElementById("navbar");
+  let scroll = window.scrollY;
+  if (scroll < 150) {
+    navbar.classList.remove("bg-nav");
+    
+  } else {
+    navbar.classList.add("bg-nav");
+  }
+}
+
+window.addEventListener("scroll", changeColor);
+
+
+
+
+
+
+/* ========================= */
+
+
+/* ================ Slider ===============  */
+
+
+$(document).ready(function() {
+  
+  $(".skitter-large").skitter(
+    {
+      itemes:1
+    }
+  );
+});
+
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    itemes:3,
+    autoplay:true,
+    autoplayTimeout:1000,
+    margin:10,
+    loop:true
+  });
+});
+
+
+
+
+
+
+/* ===========  text =============== */
+
+
 
 // let allProject = ["image/1.jpg", "image/2.jpg", "image/3.jpg"];
 // /* et allProject = [{id:1, name:"image/1.jpg" ,title:"one"}, {id:2, name:"image/2.jpg" ,title:"Two"}];
@@ -188,64 +254,3 @@ function moveNegative() {
 
 // let data = JSON.parse(localStorage.getItem('data')) || [];
 // console.log(data);
-
-
-
-
-
-
-
-
-
-
-
-/* Change Backgroung  */
-
-function changeColor() {
-  let navbar = document.getElementById("navbar");
-  let scroll = window.scrollY;
-  if (scroll < 150) {
-    navbar.classList.remove("bg-nav");
-    
-  } else {
-    navbar.classList.add("bg-nav");
-  }
-}
-
-window.addEventListener("scroll", changeColor);
-
-
-
-/*  */
-
-
-
-/* ========================= */
-
-
-/* ================ Slider ===============  */
-
-
-$(document).ready(function() {
-  
-  $(".skitter-large").skitter(
-    {
-      itemes:1
-    }
-  );
-});
-
-
-$(document).ready(function(){
-  $(".owl-carousel").owlCarousel({
-    itemes:3,
-    autoplay:true,
-    autoplayTimeout:1000,
-    margin:10,
-    loop:true
-  });
-});
-
-
-
-
